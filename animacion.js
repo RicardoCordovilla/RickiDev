035536f7a58dce6a703016d6e3aa2820c2810749
RicardoCordovilla <ricardocordovillaf@gmail.com>
2022-07-12 01:57:11 -0500

    agrgado scroll reveal

--- animacion.js
+++ animacion.js
@@ -1,3 +1,4 @@
+ScrollReveal({ reset: true });
 const btnBar=document.getElementById('burguer')
 const menuLateral=document.getElementById('menuLateral')
 const btnSection=document.getElementById('menuLateral')
@@ -13,3 +14,51 @@ btnSection.addEventListener('click',()=>{
     console.log('click en menu')
 })
 
+ScrollReveal().reveal('.headline',{
+    delay:300,
+    duration:1000,
+    easing: 'ease-out',
+    // distance: '100%',
+    opacity: 0,
+    origin:'right',
+    interval:300,
+});
+
+ScrollReveal().reveal('#revelImg-1',{
+    delay:100,
+    duration:500,
+    easing: 'ease-out',
+    distance: '100%',
+    opacity: 0,
+    origin:'bottom',
+    interval:100,
+});
+ScrollReveal().reveal('#revelImg-2',{
+    delay:100,
+    duration:400,
+    easing: 'ease-in-out',
+    distance: '100%',
+    opacity: 0,
+    origin:'top',
+    interval:200,
+});
+ScrollReveal().reveal('#revelImg-3',{
+    delay:400,
+    duration:500,
+    easing: 'ease-out',
+    distance: '100%',
+    opacity: 0,
+    origin:'bottom',
+    interval:300,
+});
+
+
+ScrollReveal().reveal('.revelImg1',{
+    delay:100,
+    duration:500,
+    easing: 'ease-in-out',
+    // distance: '50%',
+    opacity: 0,
+    // origin:'bottom',
+    interval:200,
+});
