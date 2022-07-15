7ff53b9fc1ca843bb888ec052ea06e3cb1335d02
RicardoCordovilla <ricardocordovillaf@gmail.com>
2022-07-15 14:30:24 -0500

    cambio en el navbar y ajuste para pantallas xxl

--- animacion.js
+++ animacion.js
@@ -1,18 +1,5 @@
 ScrollReveal({ reset: true });
-const btnBar=document.getElementById('burguer')
-const menuLateral=document.getElementById('menuLateral')
-const btnSection=document.getElementById('menuLateral')
 
-btnBar.addEventListener('click',()=>{
-    menuLateral.classList.toggle('menuShow')
-    console.log('menu')
-})
-
-
-btnSection.addEventListener('click',()=>{
-    menuLateral.classList.remove('menuShow')
-    console.log('click en menu')
-})
 
 ScrollReveal().reveal('.headline',{
     delay:300,
